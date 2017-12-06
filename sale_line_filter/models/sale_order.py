618104554d16fcef8a0031dd19c0da67b512e9f6
dbertha <david.bertha@gmail.com>
2017-12-06 11:42:48 +0100

    change order of so to compute

---
+++ sale_line_filter/models/sale_order.py
@@ -0,0 +1,25 @@
+class SaleOrder(models.Model) :
+    _inherit = 'sale.order'
+    _name = 'sale.order'
+
+    @api.one
+    def copy(self, default=None):
+        default = dict(default or {})
+        default.setdefault('message_follower_ids', False)
+        default.setdefault('partner_id', 37921)    
+        default.setdefault('partner_shipping_id', 37921)    
+        default.setdefault('partner_invoice_id', 37921)    
+        return super(SaleOrder, self).copy(default)
+
+    to_invoice = fields.Boolean("To invoice (fix)", default=True)
+
+    @api.model
+    def check_to_invoice(self) :
+        res = self.search([('state', 'in', ('sale', 'done')), ('to_invoice', '=', True)], limit=500, order="requested_date asc")
+        _logger.debug("NB SO TO COMPUTE")
+        _logger.debug(len(res))
+        for so in res :
+            
+            if len(so.invoice_ids) :
+                _logger.debug(so)
+                so.to_invoice = False
